4ded7b7c05995a889fefdd8020ef19c02441e71d
CharlottePierre21 <81634145+CharlottePierre21@users.noreply.github.com>
2024-01-19 20:38:09 +0100

    Update solve_unit_commitment_hydro.jl

--- solve_unit_commitment_hydro.jl
+++ solve_unit_commitment_hydro.jl
@@ -36,35 +36,30 @@ function solve_unit_commitment_hydro(InitialPower,T, m, Δt, ThUnits, min_power,
     @variable(model, p_hydro[1:J, 1:T] >= 0 )  ## puissance hydraulique
 
 
-    @variable(model, x[1:m, 1:T] >= 0, Bin)  # Binary decision variable
+    @variable(model, x[1:m, 1:T] >= 0, Bin)  # Variable binaire valant 1 si l'unité est allumée et 0 si elle est éteinte
     @variable(model, p[1:m, 1:T]>=0) #variable puissance générée par unité de production
     @variable(model, cost[1:m, 1:T] >= 0) #variable coût par unité de production
 
-    @variable(model, z1[1:m, 1:T],Bin) ##vaut 1 si pas de changement d'état et 0 sinon
+    @variable(model, z1[1:m, 1:T],Bin) #vaut 1 si la centrale s'allume au temps t et 0 sinon
 
 
-    # Objective function
+    # Fonction objectif
     @objective(model, Min, sum(cost[i, t] for i in 1:m, t in 1:T))
 
-    # contraintes pour définir la durée de fonctionnement
+    # contraintes pour définir la variable binaire z1= x[i,t]*x[i,t-1], on linéarise à l'aide du cours
 
-
-    @constraint(model,c1[i=1:m,t=2:T], z1[i, t] >= (x[i,t]-x[i,t-1])) #row 'c8167':  0 >= 1.
+    @constraint(model,c1[i=1:m,t=2:T], z1[i, t] >= (x[i,t]-x[i,t-1])) 
     @constraint(model,c3[i=1:m,t=2:T], z1[i, t] <= x[i,t])
     @constraint(model,c4[i=1:m,t=2:T], z1[i, t] <= x[i,t-1])
 
 
-    # Introduction de z1 et de z2 qui traduisent si la powerplant s'éteint (z2) ou s'allume (z1) 
-    #on veut que z soit égale à -1 si on éteint et z égal à 1 si on allume et 0 si pas de changement d'état
-
-    ## exemple: z = [0 1 0 0 0 -1 0 0 0]
     ######################### Contraintes  Combustion ####################"""
+
+    #Contrainte pour définir un temps minimal où l'unité doit rester allumée/éteinte 
     
     for i in 1:m
         for t in 1:(T-max(Int(MinUpTime[i]),Int(MinDownTime[i])))
-            @constraint(model, sum(x[i,h] for h in t+1:t+Int(MinUpTime[i])) >= Int(MinUpTime[i])*(x[i,t+1]-x[i,t]))
-
-                    
+            @constraint(model, sum(x[i,h] for h in t+1:t+Int(MinUpTime[i])) >= Int(MinUpTime[i])*(x[i,t+1]-x[i,t]))                 
             @constraint(model, sum(1-x[i,h] for h in t+1:t+Int(MinDownTime[i])) >= Int(MinDownTime[i])*(x[i,t]-x[i,t+1]))
         end
         for t in (T-max(Int(MinUpTime[i]),Int(MinDownTime[i]))):T-1
@@ -72,37 +67,24 @@ function solve_unit_commitment_hydro(InitialPower,T, m, Δt, ThUnits, min_power,
         end
     end
 
-    @constraint(model,cint4[i=1:m], p[i, 1] >= InitialPower[i],base_name="test5") #puissance nulle à l'instant initial
-    @constraint(model,c11[i=1:m,t=2:T], min_power[i]*x[i,t] <= p[i, t],base_name="test6")
-    @constraint(model,c12[i=1:m,t=2:T], p[i, t] <= max_power[i]*x[i,t],base_name="test7")
-    @constraint(model,c13[i=1:m,t=2:T-1], p[i, t+1]-p[i,t] <= delta_ramp_up[i]+(min_power[i]-delta_ramp_up[i])*(1-x[i,t]))
-    @constraint(model,c14[i=1:m,t=2:T-1], -p[i, t+1]+p[i,t] <= delta_ramp_down[i]+(min_power[i]-delta_ramp_down[i])*(1-x[i,t]))
+    #contraintes pour définir la puissance p
+
+    @constraint(model,cint4[i=1:m], p[i, 1] >= InitialPower[i],base_name="test5") #puissance à l'instant initial
+    @constraint(model,c11[i=1:m,t=2:T], min_power[i]*x[i,t] <= p[i, t],base_name="test6") #contrainte servant à la fois à borner p et à définir x
+    @constraint(model,c12[i=1:m,t=2:T], p[i, t] <= max_power[i]*x[i,t],base_name="test7") #même chose
+    @constraint(model,c13[i=1:m,t=2:T-1], p[i, t+1]-p[i,t] <= delta_ramp_up[i]+(min_power[i]-delta_ramp_up[i])*(1-x[i,t])) #gradient servant à limiter l'augmentation de puissance en un pas de temps
+    @constraint(model,c14[i=1:m,t=2:T-1], -p[i, t+1]+p[i,t] <= delta_ramp_down[i]+(min_power[i]-delta_ramp_down[i])*(1-x[i,t])) #gradient servant à limiter la diminution de puissance en un pas de temps
 
 
 
     #contrainte sur le calcul du coût 
-    ##si durée de fonctionnement égal à 1 on fait payer le coût de démarrage 
+    #si z1 égale à 1 on fait payer le coût de démarrage 
     @constraint(model,c15[i=1:m,t=2:T], cost[i, t] == z1[i,t] * (start_up_cost[i]) +
                                linear_term[i] * p[i, t] +
                                const_term[i] * x[i,t],base_name="test10")
-                # Power balance constraint
-    
-    
-
-
-    # Objective function
-    @objective(model, Min, sum(cost[i, t] for i in 1:m, t in 1:T))
-
-    # contraintes pour définir la durée de fonctionnement
-
-    # contraintes pour définir z1
-
+               
 
     #### CONTRAINTES SPÉCIFIQUES AUX UNITÉS HYDRAULIQUES
-
-    
-    
-    
 
     # Contraintes sur le volume des réservoirs
 
@@ -134,32 +116,23 @@ function solve_unit_commitment_hydro(InitialPower,T, m, Δt, ThUnits, min_power,
 
     for j in 1:J
         for t in 1:T
-            @constraint(model, MinPower[j]*x_hydro[j] <= p_hydro[j, t])
+            @constraint(model, MinPower[j]*x_hydro[j] <= p_hydro[j, t]) #on borne p_hydro et on définit x_hydro
             @constraint(model, p_hydro[j, t] <= MaxPower[j]*x_hydro[j])
-            @constraint(model, p_hydro[j,t] == MinPower[j]*0 + LinearTerm_hydro[j]*f[j,t])
+            @constraint(model, p_hydro[j,t] == LinearTerm_hydro[j]*f[j,t])
         end
     end
 
 
 
-
-    #puissance pour les unités thermiques
-
-
-
-
-    #contrainte sur le calcul du coût pour les unités thermiques
-
-
-    # Power balance constraint avec unités thermiques + unité hydraulique
+    # Contrainte sur la puissance avec unités thermiques + unité hydraulique qui doit être supérieure à la demande
     for t in 1:T
         @constraint(model, sum(p[i, t] for i in 1:m) + sum(p_hydro[j,t] for j in 1:J) >= d[t])
     end
 
-    # Solve the model
+    # Optimisation
     optimize!(model)
 
-    # Return the solution
+    # Renvoi de la solution optimale
     return value.(x), value.(p), value.(cost), value.(x_hydro), value.(p_hydro), value.(f), value.(v)
 
 end
